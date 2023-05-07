a3098854ca9516cc0f16b2afcc6154ed6723a405
Johannes Wüller <johanneswueller@gmail.com>
2023-05-07 17:58:07 +0200

    Fix expected type

--- JWueller.Jellyfin.OnePace/IdentifierUtil.cs
+++ JWueller.Jellyfin.OnePace/IdentifierUtil.cs
@@ -4,7 +4,7 @@ namespace JWueller.Jellyfin.OnePace;
 
 internal static class IdentifierUtil
 {
-    public static bool MatchesOnePaceInvariantTitle(string candidate)
+    public static bool MatchesOnePaceInvariantTitle(string? candidate)
     {
         return candidate != null && Regex.IsMatch(candidate, @"\bOne\s*Pace\b", RegexOptions.IgnoreCase);
     }
